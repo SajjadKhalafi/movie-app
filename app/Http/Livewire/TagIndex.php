86264ffa0cd8a6eeef2a845f95fbbfd0e191a2ed
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-12-31 22:56:23 +0330

    complete CRUD tag management system

--- app/Http/Livewire/TagIndex.php
+++ app/Http/Livewire/TagIndex.php
@@ -2,10 +2,78 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Tag;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class TagIndex extends Component
 {
+    public $tagName;
+    public $showTagModal = false;
+    public $tags = [];
+    public $tagId;
+
+    public function mount()
+    {
+        $this->tags = Tag::all();
+    }
+
+    public function showCreateModal()
+    {
+        $this->reset(['tagId' , 'tagName']);
+        $this->showTagModal = true;
+    }
+
+    public function closeTagModal()
+    {
+        $this->showTagModal = false;
+    }
+
+    public function createTag()
+    {
+        Tag::create([
+            'tag_name' => $this->tagName,
+            'slug' => Str::slug($this->tagName)
+        ]);
+        $this->reset();
+        session()->flash('flash.banner', 'tag created successfully!');
+        session()->flash('flash.bannerStyle', 'success');
+        $this->tags = Tag::all();
+    }
+
+    public function showEditModal($tagId)
+    {
+        $this->reset(['tagName']);
+        $this->tagId = $tagId;
+        $tag = Tag::find($tagId);
+        $this->tagName = $tag->tag_name;
+        $this->showTagModal = true;
+    }
+
+    public function updateTag()
+    {
+        $tag = Tag::findOrFail($this->tagId);
+        $tag->update([
+            'tag_name' => $this->tagName,
+            'slug' => Str::slug($this->tagName),
+        ]);
+        $this->reset();
+        session()->flash('flash.banner', 'tag updated successfully!');
+        session()->flash('flash.bannerStyle', 'success');
+        $this->tags = Tag::all();
+        $this->showTagModal = false;
+    }
+
+    public function deleteTag($tagId)
+    {
+        $tag = Tag::findOrFail($tagId);
+        $tag->delete();
+        $this->reset();
+        session()->flash('flash.banner', 'tag deleted successfully!');
+        session()->flash('flash.bannerStyle', 'success');
+        $this->tags = Tag::all();
+    }
+
     public function render()
     {
         return view('livewire.tag-index');
--- resources/views/livewire/tag-index.blade.php
+++ resources/views/livewire/tag-index.blade.php
@@ -1,6 +1,9 @@
 <section class="container mx-auto p-6 font-mono">
+    @if(\Illuminate\Support\Facades\Session::has('flash.banner'))
+        <x-m-flash-banner />
+    @endif
     <div class="w-full flex mb-4 p-2 justify-end">
-        <x-jet-button>
+        <x-jet-button wire:click="showCreateModal">
             Create Tag
         </x-jet-button>
     </div>
@@ -10,26 +13,81 @@
                 <thead>
                 <tr class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
                     <th class="px-4 py-3">Title</th>
-                    <th class="px-4 py-3">Date</th>
-                    <th class="px-4 py-3">Rating</th>
-                    <th class="px-4 py-3">Public</th>
+                    <th class="px-4 py-3">Slug</th>
                     <th class="px-4 py-3">Manage</th>
                 </tr>
                 </thead>
                 <tbody class="bg-white">
-                <tr class="text-gray-700">
-                    <td class="px-4 py-3 border">
-                        Title Here
-                    </td>
-                    <td class="px-4 py-3 text-ms font-semibold border">Date Here</td>
-                    <td class="px-4 py-3 text-xs border">
-                        <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> Rating Here </span>
-                    </td>
-                    <td class="px-4 py-3 text-sm border">Public Here</td>
-                    <td class="px-4 py-3 text-sm border">Edit/Delete</td>
-                </tr>
+                @forelse($tags as $tag)
+                    <tr class="text-gray-700">
+                        <td class="px-4 py-3 border">
+                            {{ $tag->tag_name }}
+                        </td>
+                        <td class="px-4 py-3 border">
+                            {{ $tag->slug }}
+                        </td>
+                        <td class="px-4 py-3 text-sm border">
+                            <x-m-button wire:click="showEditModal({{ $tag->id }})"
+                                          class="bg-blue-500 hover:bg-blue-700 font-bold">Edit
+                            </x-m-button>
+                            <x-m-button wire:click="deleteTag({{ $tag->id }})"
+                                          class="bg-red-500 hover:bg-red-700 font-bold">Delete
+                            </x-m-button>
+                        </td>
+                    </tr>
+                @empty
+                    <tr class="text-gray-700">
+                        <td class="px-4 py-3 border" colspan="3">
+                            Empty
+                        </td>
+                    </tr>
+                @endforelse
                 </tbody>
             </table>
         </div>
     </div>
+    <x-jet-dialog-modal wire:model="showTagModal">
+        <x-slot name="title">
+            @if($tagId)
+                Update Tag
+            @else
+                Create Tag
+            @endif
+        </x-slot>
+        <x-slot name="content">
+            <div class="mt-10 sm:mt-0">
+                <div class="mt-5 md:col-span-2 md:mt-0">
+                    <form>
+                        <div class="overflow-hidden shadow sm:rounded-md">
+                            <div class="bg-white px-4 py-5 sm:p-6">
+                                <div class="grid grid-cols-6 gap-6">
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label for="tagName" class="block text-sm font-medium text-gray-700">Tag
+                                            name</label>
+                                        <input type="text" wire:model="tagName" autocomplete="given-name"
+                                               class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </x-slot>
+        <x-slot name="footer">
+            @if($tagId)
+                <x-m-button wire:click="updateTag">
+                    Update
+                </x-m-button>
+            @else
+                <x-m-button wire:click="createTag">
+                    Create
+                </x-m-button>
+            @endif
+            <x-jet-button wire:click="closeTagModal">
+                Close
+            </x-jet-button>
+
+        </x-slot>
+    </x-jet-dialog-modal>
 </section>
