94c2a9ead382d5b22cb6acc2723bed60461423f8
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2023-01-01 19:51:54 +0330

    complete CRUD cast management system

--- app/Http/Livewire/CastIndex.php
+++ app/Http/Livewire/CastIndex.php
@@ -2,12 +2,91 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Cast;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+use Livewire\WithPagination;
 use Livewire\Component;
 
 class CastIndex extends Component
 {
+    use WithPagination;
+
+    protected $key = '372bd4e6b1011f0918d4409a850fc3e3';
+    public $castTMDBid, $castName, $castPosterPath;
+    public $showCastModal = false;
+    public $castId;
+    protected $rules = [
+        'castName' => 'required',
+        'castPosterPath' => 'required'
+    ];
+
+    public function generateCast()
+    {
+        $newCast = Http::get("https://api.themoviedb.org/3/person/" . $this->castTMDBid . "?api_key=" . $this->key)->json();
+        $cast = Cast::where('tmdb_id', $this->castTMDBid)->first();
+        if (!$cast) {
+            Cast::create([
+                'tmdb_id' => $newCast['id'],
+                'name' => $newCast['name'],
+                'slug' => Str::slug($newCast['name']),
+                'poster_path' => $newCast['profile_path'],
+            ]);
+            $this->reset();
+            session()->flash('flash.banner', 'cast created successfully!');
+            session()->flash('flash.bannerStyle', 'success');
+        } else {
+            $this->reset();
+            session()->flash('flash.banner', 'cast already exist!');
+            session()->flash('flash.bannerStyle', 'danger');
+        }
+    }
+
+
+    public function showEditModal($id)
+    {
+        $this->castId = $id;
+        $this->loadCast();
+        $this->showCastModal = true;
+    }
+
+    public function loadCast()
+    {
+        $cast = Cast::findOrFail($this->castId);
+        $this->castName = $cast->name;
+        $this->castPosterPath = $cast->poster_path;
+    }
+
+    public function closeCastModal()
+    {
+        $this->reset();
+    }
+
+    public function updateCast()
+    {
+        $this->validate();
+        $cast = Cast::findOrFail($this->castId);
+        $cast->update([
+            'name' => $this->castName,
+            'poster_path' => $this->castPosterPath,
+        ]);
+        session()->flash('flash.banner', 'cast updated successfully!');
+        session()->flash('flash.bannerStyle', 'success');
+        $this->reset();
+    }
+
+    public function deleteCast($castId)
+    {
+        Cast::findOrFail($castId)->delete();
+        session()->flash('flash.banner', 'cast deleted successfully!');
+        session()->flash('flash.bannerStyle', 'success');
+        $this->reset();
+    }
+
     public function render()
     {
-        return view('livewire.cast-index');
+        return view('livewire.cast-index', [
+            'casts' => Cast::paginate(5)
+        ]);
     }
 }
--- resources/views/livewire/cast-index.blade.php
+++ resources/views/livewire/cast-index.blade.php
@@ -1,35 +1,108 @@
 <section class="container mx-auto p-6 font-mono">
+    @if(\Illuminate\Support\Facades\Session::has('flash.banner'))
+        <x-m-flash-banner/>
+    @endif
     <div class="w-full flex mb-4 p-2 justify-end">
-        <x-jet-button>
-            Create Cast
-        </x-jet-button>
+        <form class="flex shadow bg-white rounded-md m-2 p-2">
+            <div class="p-1 flex items-center">
+                <label for="castTMDBid" class="block text-sm font-medium text-gray-700 md:mr-4">Cast Tmdb Id</label>
+                <div class="relative rounded-md shadow-sm">
+                    <input wire:model="castTMDBid" id="tmdb_id_g" name="tmdb_id_g"
+                           class="px-3 py-2 border border-gray-300 rounded" placeholder="Cast TMDB ID"/>
+                </div>
+            </div>
+            <div class="p-1">
+                <button wire:click="generateCast" type="button"
+                        class="inline-flex items-center justify-center py-2 px-4 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-green-700 transition duration-150 ease-in-out disabled:opacity-50">
+                    <span>Generate</span>
+                </button>
+            </div>
+        </form>
     </div>
     <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
         <div class="w-full overflow-x-auto">
             <table class="w-full">
                 <thead>
                 <tr class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
-                    <th class="px-4 py-3">Title</th>
-                    <th class="px-4 py-3">Date</th>
-                    <th class="px-4 py-3">Rating</th>
-                    <th class="px-4 py-3">Public</th>
+                    <th class="px-4 py-3">Name</th>
+                    <th class="px-4 py-3">Slug</th>
+                    <th class="px-4 py-3">Poster</th>
                     <th class="px-4 py-3">Manage</th>
                 </tr>
                 </thead>
                 <tbody class="bg-white">
-                <tr class="text-gray-700">
-                    <td class="px-4 py-3 border">
-                        Title Here
-                    </td>
-                    <td class="px-4 py-3 text-ms font-semibold border">Date Here</td>
-                    <td class="px-4 py-3 text-xs border">
-                        <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> Rating Here </span>
-                    </td>
-                    <td class="px-4 py-3 text-sm border">Public Here</td>
-                    <td class="px-4 py-3 text-sm border">Edit/Delete</td>
-                </tr>
+                @forelse($casts as $cast)
+                    <tr class="text-gray-700">
+                        <td class="px-4 py-3 border">
+                            {{ $cast->name }}
+                        </td>
+                        <td class="px-4 py-3 text-ms font-semibold border">{{ $cast->slug }}</td>
+                        <td class="px-4 py-3 text-sm border">{{ $cast->poster_path }}</td>
+                        <td class="px-4 py-3 text-sm border">
+                            <x-m-button wire:click="showEditModal({{ $cast->id }})"
+                                        class="bg-blue-500 hover:bg-blue-700 font-bold">Edit
+                            </x-m-button>
+                            <x-m-button wire:click="deleteCast({{ $cast->id }})"
+                                        class="bg-red-500 hover:bg-red-700 font-bold">Delete
+                            </x-m-button>
+                        </td>
+                    </tr>
+                @empty
+                    <tr class="text-gray-700">
+                        <td class="px-4 py-3 border" colspan="4">
+                            Empty
+                        </td>
+                    </tr>
+                @endforelse
                 </tbody>
             </table>
+            <div>
+                {{ $casts->links() }}
+            </div>
         </div>
     </div>
+    <x-jet-dialog-modal wire:model="showCastModal">
+        <x-slot name="title">
+            Update Cast
+        </x-slot>
+        <x-slot name="content">
+            <div class="mt-10 sm:mt-0">
+                <div class="mt-5 md:col-span-2 md:mt-0">
+                    <form>
+                        <div class="overflow-hidden shadow sm:rounded-md">
+                            <div class="bg-white px-4 py-5 sm:p-6">
+                                <div class="grid grid-cols-6 gap-6">
+
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label for="tagName" class="block text-sm font-medium text-gray-700">Cast
+                                            name</label>
+                                        <input type="text" wire:model="castName" autocomplete="given-name"
+                                               class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                                        @error('castName') {{ $message }} @enderror
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label for="tagName" class="block text-sm font-medium text-gray-700">Cast
+                                            PosterPath</label>
+                                        <input type="text" wire:model="castPosterPath" autocomplete="given-name"
+                                               class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                                        @error('castPosterPath') {{ $message }} @enderror
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </x-slot>
+        <x-slot name="footer">
+            <x-m-button wire:click="updateCast">
+                Update
+            </x-m-button>
+            <x-jet-button wire:click="closeCastModal">
+                Close
+            </x-jet-button>
+
+        </x-slot>
+    </x-jet-dialog-modal>
 </section>
